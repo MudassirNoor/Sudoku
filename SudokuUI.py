acd83c8791c21a7aca3070f65a2f43cc97c00e7a
Mudassir Noor <mnoor@sfu.ca>
2020-01-04 19:47:39 -0800

    Added draw grid function

--- SudokuUI.py
+++ SudokuUI.py
@@ -17,13 +17,12 @@ class GameWindow(Toplevel):
     def __init__(self, original):
         self.original_frame = original
         Toplevel.__init__(self)
-        self.geometry("500x500")
+        # self.geometry("500x500")
         self.sideBar = Frame(self, bg = "red", bd = 3)
         self.gameFrame = Frame(self, bg = "blue", bd = 3)
 
         self.makeSideBar()
-        self.makeGameFrame()
-
+        self.drawGrid()
 
     def makeSideBar(self):
         self.sideBar.grid(row = 8, column = 0, columnspan = 9)
@@ -33,14 +32,29 @@ class GameWindow(Toplevel):
         quitGameButton.grid(column = 2, row = 0)
         #TODO: perhaps add a timer window
 
-    def makeGameFrame(self):
+    def drawGrid(self):
         self.gameFrame.grid(row = 0, rowspan = 8, columnspan = 8)
         selectedDifficulty = self.original_frame.difficulty
+
+        canvas = Canvas(self, height = 450, width = 450,  bg = "white")
+        canvas.grid(row = 0, column = 0, rowspan = 8, columnspan = 8)
+
+        # verticalIteration = 0
+        yFinal = 0
+        for yInitial in range(0, 450, 50):
+            xFinal = 0
+            yFinal += 50
+            for xInitial in range(0, 450, 50):
+                xFinal += 50
+                canvas.create_rectangle(xInitial, yInitial, xFinal, yFinal)
+
+        for xInitial in (150, 300):
+            canvas.create_line(0, xInitial, 450, xInitial,  fill = "red", width = 2)
+            canvas.create_line(xInitial, 0, xInitial, 450,  fill = "red", width = 2)
 
     def close(self):
         self.destroy()
         self.original_frame.makeVisible()
-
 
 class Sudoku(object):
 
